fd9b53b16d27fbba8f442e0810b79f6f21d59ce8
Pooja Vishnoi <pooja.vishnoi@gmail.com>
2025-10-29 13:35:32 +0530

    "debugging"

--- agents/__init__.py
+++ agents/__init__.py
@@ -10,18 +10,20 @@ print(">>> sys.path:", sys.path)
 print(">>> contents of agents folder:", os.listdir(os.path.dirname(__file__)))
 
 # from .root_agent import StartupAnalyzerAgent
+from agents.root_agent import StartupAnalyzerAgent
 
-try:
-    from .root_agent import StartupAnalyzerAgent
-except ImportError:
-    # fallback if Streamlit changes package context
-    import importlib.util, os, sys
-    module_path = os.path.join(os.path.dirname(__file__), "root_agent.py")
-    spec = importlib.util.spec_from_file_location("root_agent", module_path)
-    root_agent = importlib.util.module_from_spec(spec)
-    sys.modules["root_agent"] = root_agent
-    spec.loader.exec_module(root_agent)
-    StartupAnalyzerAgent = root_agent.StartupAnalyzerAgent
+
+# try:
+#     from .root_agent import StartupAnalyzerAgent
+# except ImportError:
+#     # fallback if Streamlit changes package context
+#     import importlib.util, os, sys
+#     module_path = os.path.join(os.path.dirname(__file__), "root_agent.py")
+#     spec = importlib.util.spec_from_file_location("root_agent", module_path)
+#     root_agent = importlib.util.module_from_spec(spec)
+#     sys.modules["root_agent"] = root_agent
+#     spec.loader.exec_module(root_agent)
+#     StartupAnalyzerAgent = root_agent.StartupAnalyzerAgent
 
 from .data_processing_agent import DataProcessingAgent
 from .calculation_agent import CalculationAgent
