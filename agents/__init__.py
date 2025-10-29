85cf67008911d8bff001f2b42bf99c0465f04991
Pooja Vishnoi <pooja.vishnoi@gmail.com>
2025-10-29 13:22:11 +0530

    debugging issue

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,6 +2,12 @@
 ADK Agents Package
 Startup Analyzer Agent Framework
 """
+import os, sys
+print(">>> INIT DEBUG:")
+print(">>> __file__:", __file__)
+print(">>> cwd:", os.getcwd())
+print(">>> sys.path:", sys.path)
+print(">>> contents of agents folder:", os.listdir(os.path.dirname(__file__)))
 
 from .root_agent import StartupAnalyzerAgent
 from .data_processing_agent import DataProcessingAgent
